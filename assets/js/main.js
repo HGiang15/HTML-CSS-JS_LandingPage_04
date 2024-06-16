4267014548ef859781e89b5f4c4cceb411e47984
HGiang15 <gianghoang150503@gmail.com>
2024-06-16 10:28:56 +0700

    update js

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+const links = document.querySelectorAll(".navbar__link");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((link) => link.classList.remove("navbar__link--active"));
+    link.classList.add("navbar__link--active");
+  });
+});
+
+const dots = document.querySelectorAll(".review__dots .review__dot");
+const reviews = document.querySelectorAll(
+  ".review .review__list .feedback-item"
+);
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    dots.forEach((dot) => dot.classList.remove("review__dot--active"));
+    dot.classList.add("review__dot--active");
+  });
+});
+
+// Chuyển item bằng dot và btn
+document.addEventListener("DOMContentLoaded", () => {
+  const reviewList = document.querySelector(".review__list");
+  const reviewItems = document.querySelectorAll(".review-item");
+  const prevButton = document.querySelector(".preview__control:first-child");
+  const nextButton = document.querySelector(".preview__control:last-child");
+  const dots = document.querySelectorAll(".review__dot");
+
+  let currentIndex = 0;
+
+  function updateDots(index) {
+    dots.forEach((dot) => dot.classList.remove("review__dot--active"));
+    dots[index].classList.add("review__dot--active");
+  }
+
+  function updateReview() {
+    const offset = -currentIndex * 100; // Tính toán độ dịch chuyển
+    reviewList.style.transform = `translateX(${offset}%)`;
+    updateDots(currentIndex);
+  }
+
+  prevButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = reviewItems.length - 1; // Quay lại phần tử cuối cùng
+    }
+    updateReview();
+  });
+
+  nextButton.addEventListener("click", () => {
+    if (currentIndex < reviewItems.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0; // Quay lại phần tử đầu tiên
+    }
+    updateReview();
+  });
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      currentIndex = index;
+      updateReview();
+    });
+  });
+});
